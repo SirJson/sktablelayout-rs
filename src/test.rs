cb5410c0794e391f27a94356ba9f12bfb3e4bc2a
Joshua Cearley <joshua.cearley@gmail.com>
2018-08-17 19:13:11 -0500

    More complex test for cell padding.

--- src/test.rs
+++ src/test.rs
@@ -129,3 +129,65 @@ fn padded_big_cell() {
                     .padding_all(16.0));
     engine.impose(64.0, 64.0);
 }
+
+#[test]
+fn padded_multilayout() {
+    let mut engine = TableLayout::new();
+    engine.with_cell(CellProperties::new()
+                    .callback(Box::new(|x, y, w, h| {
+                        println!("{} {} {} {}", x, y, w, h);
+                        assert_eq!(x, 16.0);
+                        assert_eq!(y, 16.0);
+                        // these are expand, not fill, so the
+                        // cell takes up extra space but the
+                        // child item actually doesn't use it
+                        assert_eq!(w, 32.0);
+                        assert_eq!(h, 32.0);
+                    }))
+                    .anchor_right()
+                    .anchor_bottom()
+                    .padding_all(16.0)
+                    .preferred_size(Size{width: 32.0, height: 32.0}));
+    engine.with_cell(CellProperties::new()
+                    .callback(Box::new(|x, y, w, h| {
+                        println!("{} {} {} {}", x, y, w, h);
+                        // first column is neither expand nor
+                        // fill horizontal, so should be packed
+                        // as preferred width
+                        assert_eq!(x, 64.0);
+                        assert_eq!(y, 0.0);
+                        // these are expand, not fill, so the
+                        // cell takes up extra space but the
+                        // child item actually doesn't use it
+                        assert_eq!(w, 64.0);
+                        assert_eq!(h, 64.0);
+                    }))
+                    .anchor_top()
+                    .anchor_left()
+                    .expand_horizontal()
+                    .preferred_size(Size{width: 64.0, height: 64.0}));
+    engine.with_cell(CellProperties::new()
+                    .callback(Box::new(|x, y, w, h| {
+                        println!("{} {} {} {}", x, y, w, h);
+                        assert_eq!(y, 0.0);
+                        assert_eq!(h, 64.0);
+                    }))
+                    .anchor_right()
+                    .expand_horizontal()
+                    .fill_horizontal()
+                    .preferred_size(Size{width: 64.0, height: 64.0}));
+    engine.with_row();
+    engine.with_cell(CellProperties::new()
+                    .callback(Box::new(|x, y, w, h| {
+                        println!("{} {} {} {}", x, y, w, h);
+                        assert_eq!(x, 0.0);
+                        assert_eq!(y, 240.0 - 64.0);
+                        assert_eq!(w, 320.0);
+                    }))
+                    .colspan(3)
+                    .expand_vertical()
+                    .anchor_bottom()
+                    .fill_horizontal()
+                    .preferred_size(Size{width: 64.0, height: 64.0}));
+    engine.impose(320.0, 240.0);
+}
